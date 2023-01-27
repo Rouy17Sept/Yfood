765026e54b777e64cf8406d2b2cb92f03450bb9b
Rouy17Sept <86048000+Rouy17Sept@users.noreply.github.com>
2023-01-27 16:28:11 +0100

    Add files via upload

---
+++ food-order/partials-front/footer.php
@@ -0,0 +1,28 @@
+<!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-center 25px">
+            <ul>
+                <li>
+                    <a href="https://nl-nl.facebook.com/youri.voogd"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/youri078/?hl=nl"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://nl.linkedin.com/in/youri-jilderda-570a43167"><img src="https://img.icons8.com/color/48/000000/linkedin-circled--v1.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="https://yjilderda.nl/">Youri Jilderda</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
